46f4acb90448b9a97e54cceb091ae64cb47e962a
somin3415 <somin3415@gmail.com>
2021-12-17 13:39:10 +0900

    Initial Commit

---
+++ Moda/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moda
+//
+//  Created by 신소민 on 2021/12/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moda/ModaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModaApp.swift
+//  Moda
+//
+//  Created by 신소민 on 2021/12/17.
+//
+
+import SwiftUI
+
+@main
+struct ModaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
