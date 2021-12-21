8e99a640bab1d07331e716f1e084962265b0de28
somin3415 <somin3415@gmail.com>
2021-12-21 14:02:52 +0900

    feat: Update home view

--- Moda/Screens/HomeView.swift
+++ Moda/Screens/HomeView.swift
@@ -12,80 +12,88 @@ struct HomeView: View {
     @State private var isAnimating: Bool = false
     
     var body: some View {
-        VStack {
-            // MARK: - HEADER
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Moda")
-                        .font(.system(size: 60, weight: .bold))
-                        .foregroundColor(.accentColor)
-                    Text("walk diary")
-                        .font(.system(.title3))
-                        .fontWeight(.light)
-                        .foregroundColor(.gray)
-                        .offset(y: -5)
+        NavigationView {
+            VStack {
+                // MARK: - HEADER
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Moda")
+                            .font(.system(size: 60, weight: .bold))
+                            .foregroundColor(.accentColor)
+                        Text("walk diary")
+                            .font(.system(.title3))
+                            .fontWeight(.light)
+                            .foregroundColor(.gray)
+                            .offset(y: -5)
+                    }
+                    Spacer()
+                }
+                
+                // MARK: - CENTER
+                Spacer()
+                NavigationLink(destination: WalkingView()
+                                .navigationTitle("")
+                                .navigationBarHidden(true)) {
+                    
+                    // Click Button
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.accentColor)
+                        .frame(height: 250, alignment: .center)
+                        .overlay(alignment: .center) {
+                            Image("pattern1")
+                                .resizable()
+                                .scaleEffect(CGSize(width: 1.0, height: -1.0))
+                                .offset(x: -150, y: -120)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .opacity(0.3)
+                            
+                            Image("pattern2")
+                                .resizable()
+                                .offset(x: 120, y: -100)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .opacity(0.3)
+                            
+                            Image("bao-black-kitten-4")
+                                .resizable()
+                                .scaledToFit()
+                                .offset(y: isAnimating ? -100 : -80)
+                                .animation(.easeInOut(duration: 4).repeatForever(), value: isAnimating)
+                            
+                            Text("클릭해서 산책시작")
+                                .font(.system(size: 17, weight: .regular))
+                                .foregroundColor(.white)
+                                .offset(y: 85)
+                                .opacity(0.9)
+                        } //: OVERLAY
+                        .scaleEffect(isAnimating ? 1 : 0.5)
+                        .animation(.easeInOut(duration: 0.5), value: isAnimating)
                 }
                 Spacer()
+                
+                // MARK: - FOOTER
+                HStack(alignment: .center, spacing: 70) {
+                    // 1. List Button
+                    MenuButton(menuIcon: "list", menuTitle: "List")
+                    
+                    // 2. Chart Button
+                    MenuButton(menuIcon: "chart", menuTitle: "Chart")
+                    
+                    // 3. Later...
+                    MenuButton(menuIcon: "setting", menuTitle: "Setting")
+                    
+                } //: FOOTER
+                .padding(.bottom, 30)
+                
+            } //: VSTACK
+            .padding(.vertical, 20)
+            .padding(.horizontal, 20)
+            .onAppear {
+                isAnimating = true
             }
-            
-            // MARK: - CENTER
-            Spacer()
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.accentColor)
-                .frame(height: 250, alignment: .center)
-                .overlay(alignment: .center) {
-                    Image("pattern1")
-                        .resizable()
-                        .scaleEffect(CGSize(width: 1.0, height: -1.0))
-                        .offset(x: -150, y: -120)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                        .opacity(0.3)
-                    
-                    Image("pattern2")
-                        .resizable()
-                        .offset(x: 120, y: -100)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                        .opacity(0.3)
-                    
-                    Image("bao-black-kitten-4")
-                        .resizable()
-                        .scaledToFit()
-                        .offset(y: isAnimating ? -100 : -80)
-                        .animation(.easeInOut(duration: 4).repeatForever(), value: isAnimating)
-                    
-                    Text("클릭해서 산책시작")
-                        .font(.system(size: 17, weight: .regular))
-                        .foregroundColor(.white)
-                        .offset(y: 85)
-                        .opacity(0.9)
-                }
-                .scaleEffect(isAnimating ? 1 : 0.5)
-                .animation(.easeInOut(duration: 0.5), value: isAnimating)
-                .onTapGesture {
-                    print("tapped!")
-                }
-            Spacer()
-            
-            // MARK: - FOOTER
-            HStack(alignment: .center, spacing: 70) {
-                // 1. List Button
-                MenuButton(menuIcon: "list", menuTitle: "List")
-                
-                // 2. Chart Button
-                MenuButton(menuIcon: "chart", menuTitle: "Chart")
-                
-                // 3. Later...
-                MenuButton(menuIcon: "setting", menuTitle: "Setting")
-                
-            } //: FOOTER
-            .padding(.bottom, 30)
-            
-        } //: VSTACK
-        .padding(.vertical, 20)
-        .padding(.horizontal, 20)
-        .onAppear {
-            isAnimating = true
-        }
+            .navigationTitle("")
+            .navigationBarHidden(true)
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
