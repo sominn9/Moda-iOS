082a0a549ebf4303b57807c062d4d2442906017e
somin3415 <somin3415@gmail.com>
2022-01-01 23:20:19 +0900

    feat: Add a function to make the border round

--- Moda/Extensions/View+Extension.swift
+++ Moda/Extensions/View+Extension.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 extension View {
+    
     func endTextEditing() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
                                         to: nil, from: nil, for: nil)
+    }
+    
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
 }
