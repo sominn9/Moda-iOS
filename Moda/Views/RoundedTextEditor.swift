fbfc69992be9efa1d77ac9c3da7b0cf3dc89f67e
somin3415 <somin3415@gmail.com>
2022-01-01 23:22:11 +0900

    refactor: Remove the text var

--- Moda/Views/RoundedTextEditor.swift
+++ Moda/Views/RoundedTextEditor.swift
@@ -11,13 +11,11 @@ struct RoundedTextEditor: View {
     
     @Binding var memo: String
     
-    @State var text: String = ""
-    
     var body: some View {
         ZStack {
-            TextEditor(text: $text)
+            TextEditor(text: $memo)
                 .frame(minHeight: 90)
-                .foregroundColor(text == "메모를 입력하세요" ? .gray : .primary)
+                .foregroundColor(memo == "메모를 입력하세요" ? .gray : .primary)
                 .disableAutocorrection(true)
                 .padding(5)
                 .overlay(
@@ -25,21 +23,20 @@ struct RoundedTextEditor: View {
                        .stroke(Color.secondary, lineWidth: 2)
                 )
                 .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)) { _ in
-                    memo = text
-                    if self.text == "" {
-                        self.text = "메모를 입력하세요"
+                    if self.memo == "" {
+                        self.memo = "메모를 입력하세요"
                     }
                 }
                 .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { _ in
-                    if self.text == "메모를 입력하세요" {
-                        self.text = ""
+                    if self.memo == "메모를 입력하세요" {
+                        self.memo = ""
                     }
                 }
       
-            Text(text).opacity(0).padding(.all, 8)
+            Text(memo).opacity(0).padding(.all, 8)
                 .onAppear {
-                    if text == "" {
-                        text = "메모를 입력하세요"
+                    if memo == "" {
+                        memo = "메모를 입력하세요"
                     }
                 }
         }
